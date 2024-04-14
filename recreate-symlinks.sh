6fc04b1f365db25e49d5a0ed9a96e64917ebd857
Exidex <16986685+Exidex@users.noreply.github.com>
2024-04-14 21:44:19 +0200

    Shared config dir for all intellij-based ides

--- recreate-symlinks.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cd ./intellij/.config/JetBrains/
-find RustRover2023.3 -type f -delete
-
-cd IntelliJIdea2023.3 # source
-find . -not -path '.' -type f -name '*' -exec ln -vsr "./{}" "../RustRover2023.3/{}" ';'
-
